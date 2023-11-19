3041b850057c600fcaf31ad05a47fd5da5f28979
kimfield98 <kimfield98@gmail.com>
2023-11-19 23:53:36 +0900

    후원 버튼 생성 (구글 로그인 쿠키 테스트)

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,7 +1,22 @@
-export default function Online() {
+"use client"
+
+import React from 'react';
+import axios from 'axios';
+function DonateButton() {
+  const handleDonateClick = async () => {
+    try {
+      const response = await axios.get('/donate', {
+        withCredentials: true, // 쿠키와 인증 정보를 포함
+      });
+      console.log(response.data); // 응답 처리
+    } catch (error) {
+      console.error('Donate request failed:', error);
+    }
+  };
   return (
-    <>
-      <div>후원 페이지 입니다.</div>
-    </>
-  )
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <button onClick={handleDonateClick}>Donate</button>
+    </div>
+  );
 }
+export default DonateButton;
