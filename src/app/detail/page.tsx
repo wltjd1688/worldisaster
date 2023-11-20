c1e0dcc7498980f037290aaefc864311af079456
kimfield98 <kimfield98@gmail.com>
2023-11-21 01:36:36 +0900

    배포 시도2

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,31 +1,19 @@
 "use client"
 
-import "../globals.css"
 import LeftSidebar from "../components/LeftSidebar"
 import RightSidebar from "../components/RightSidebar"
-import React from "react";
-import { Inter } from "next/font/google";
+import "../globals.css"
 
-
-const inter = Inter({ subsets: ["latin"] });
-
-
-export function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Detail() {
   return (
-      <html lang='en'>
-        <body className={inter.className}>
-          <main className='flex flex-row'>
+    <>
+      <main className='flex flex-row'>
             <LeftSidebar />
             <section className='main-container'>
-              <div className='w-full max-w-4xl'>{children}</div>
+              <div className='w-full max-w-4xl'>상세페이지입니다.</div>
             </section>
             <RightSidebar />
           </main>
-        </body>
-      </html>
-  );
+    </>
+  )
 }
